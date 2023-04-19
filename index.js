e6c74a70ac4ab3e2e865c3959a02bf0425b78432
shudsky <67435074+shudsky@users.noreply.github.com>
2023-04-19 02:45:35 -0400

    Overhauled layout, added to portfolio section

---
+++ index.js
@@ -0,0 +1,37 @@
+$(document).ready(function()
+{
+	//Menu Opening
+	$('.hamburger-shell').click(function()//opening menu
+	{
+		$('#menu').slideToggle(300);
+		$('.top').toggleClass('rotate');
+		$('.middle').toggleClass('rotate-back');
+		$('.menu-name').toggleClass('bump');
+		$('.bg-cover').toggleClass('reveal');
+	});
+	$('.bg-cover').click(function()
+	{
+		$('#menu').slideToggle(300);
+		$('.top').toggleClass('rotate');
+		$('.middle').toggleClass('rotate-back');
+		$('.menu-name').toggleClass('bump');
+		$('.bg-cover').toggleClass('reveal');
+	})
+	
+	$('.list-a').click(function()
+	{
+		$('#menu').slideToggle(300);
+		$('.top').toggleClass('rotate');
+		$('.middle').toggleClass('rotate-back');
+		$('.menu-name').toggleClass('bump');
+		$('.bg-cover').toggleClass('reveal');
+	})
+	$('.list-b').click(function()
+	{
+		$('#menu').slideToggle(300);
+		$('.top').toggleClass('rotate');
+		$('.middle').toggleClass('rotate-back');
+		$('.menu-name').toggleClass('bump');
+		$('.bg-cover').toggleClass('reveal');
+	})
+});
